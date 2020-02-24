e0612cbd597a802acbca00da521e01182445b3ab
felicitia <felicitia2010@gmail.com>
2020-02-23 17:38:26 -0800

    fix distance

--- gui_mapper/atm/result_generator.py
+++ gui_mapper/atm/result_generator.py
@@ -63,123 +63,123 @@ def gt_equals_trans(gt_event_new, trans_event):
 
 # seq1 and seq2 are arrays of ids
 # return the levenshtein distance
-def levenshtein(seq1, seq2):
-    # delete 'NONE' events in order to calculate levenshtein distance correctly
-    events1 = copy.deepcopy(seq1)
-    events2 = copy.deepcopy(seq2)
-    for event in events1:
-        if event == 'NONE':
-            events1.remove(event)
-    for event in events2:
-        if event == 'NONE':
-            events2.remove(event)
-
-    size_x = len(events1) + 1
-    size_y = len(events2) + 1
-    matrix = numpy.zeros ((size_x, size_y))
-    for x in range(size_x):
-        matrix [x, 0] = x
-    for y in range(size_y):
-        matrix [0, y] = y
-
-    for x in range(1, size_x):
-        for y in range(1, size_y):
-            if events1[x-1] == events2[y-1]:
-                matrix [x,y] = min(
-                    matrix[x-1, y] + 1,
-                    matrix[x-1, y-1],
-                    matrix[x, y-1] + 1
-                )
-            else:
-                matrix [x,y] = min(
-                    matrix[x-1,y] + 1,
-                    matrix[x-1,y-1] + 1,
-                    matrix[x,y-1] + 1
-                )
-    # print (matrix)
-    return (matrix[size_x - 1, size_y - 1])
+# def levenshtein(seq1, seq2):
+#     # delete 'NONE' events in order to calculate levenshtein distance correctly
+#     events1 = copy.deepcopy(seq1)
+#     events2 = copy.deepcopy(seq2)
+#     for event in events1:
+#         if event == 'NONE':
+#             events1.remove(event)
+#     for event in events2:
+#         if event == 'NONE':
+#             events2.remove(event)
+#
+#     size_x = len(events1) + 1
+#     size_y = len(events2) + 1
+#     matrix = numpy.zeros ((size_x, size_y))
+#     for x in range(size_x):
+#         matrix [x, 0] = x
+#     for y in range(size_y):
+#         matrix [0, y] = y
+#
+#     for x in range(1, size_x):
+#         for y in range(1, size_y):
+#             if events1[x-1] == events2[y-1]:
+#                 matrix [x,y] = min(
+#                     matrix[x-1, y] + 1,
+#                     matrix[x-1, y-1],
+#                     matrix[x, y-1] + 1
+#                 )
+#             else:
+#                 matrix [x,y] = min(
+#                     matrix[x-1,y] + 1,
+#                     matrix[x-1,y-1] + 1,
+#                     matrix[x,y-1] + 1
+#                 )
+#     # print (matrix)
+#     return (matrix[size_x - 1, size_y - 1])
 
 
 # handling simple cases
 # only handle sign in and sign up cases, assuming resource-id always exist
-def evaluate_atm_mapping_signin_signup(src_app, tgt_app):
-    with open(os.path.join('/Users/yixue/Documents/Research/FrUITeR/Develop/ProcessedTest_CSV/', src_app + '.csv'), 'r') as test_input:
-        with open('final_results_atm.csv', 'a') as result_output:
-            writer = csv.writer(result_output, lineterminator='\n')
-            reader = csv.reader(test_input)
-            for row in reader:
-                src_test = []
-                trans_test = []
-                correct = []
-                incorrect = []
-                missed = []
-                nonExist = []
-                # only evaluate sign in and sign up
-                if 'testSignIn()' in row[0] or 'testSignUp()' in row[0]:
-                    event_array = json.loads(row[1])
-                    for event in event_array:
-                        if 'id@' in event['id_or_xpath']:
-                            src_id = event['id_or_xpath'].split('id@')[1]
-                            src_test.append(src_id)
-                            src_can = find_canonical_for_src(src_id, src_app)
-                            trans_id = get_trans_event(src_id, src_app, tgt_app)
-                            trans_can = find_canonical_for_src(trans_id, tgt_app)
-                            trans_test.append(trans_id)
-                            if trans_can != 'NONE':
-                                if src_can == trans_can:
-                                    correct.append(src_id)
-                                else:
-                                    incorrect.append(src_id)
-                            else:
-                                if check_canonical(src_can, tgt_app):
-                                    # missed case
-                                    missed.append(src_id)
-                                else:
-                                    #nonExist case
-                                    nonExist.append(src_id)
-                        else:
-                            src_test.append(event['id_or_xpath'])
-                            print ('no id found for event ', event)
-
-                    # calcuate TP, FP, FN by comparing transferred test with ground-truth test
-                    method_name = str(row[0]).split(': ')[1]
-                    gt_test = get_gt_test(tgt_app, method_name)
-                    print ('method = ', row[0])
-                    print ('gt test = ', gt_test)
-                    print ('trans test = ', trans_test)
-                    TP = set(trans_test) & set(gt_test)
-                    FP = set(trans_test) - set(gt_test)
-                    FN = set(gt_test) - set(trans_test)
-                    # output the current test case's results to the file
-                    current_result = []
-                    current_result.append(row[0])
-                    current_result.append(src_test)
-                    current_result.append(trans_test)
-                    current_result.append(gt_test)
-                    current_result.append(src_app)
-                    current_result.append(tgt_app)
-                    current_result.append('atm')
-                    current_result.append(correct)
-                    current_result.append(incorrect)
-                    current_result.append(missed)
-                    current_result.append(nonExist)
-                    current_result.append(TP)
-                    current_result.append(FP)
-                    current_result.append(FN)
-                    current_result.append(len(correct))
-                    current_result.append(len(incorrect))
-                    current_result.append(len(missed))
-                    current_result.append(len(nonExist))
-                    if (len(correct) + len(incorrect)) == 0:
-                        current_result.append('NA')
-                    else:
-                        current_result.append(len(correct)/(len(correct) + len(incorrect)))
-                    if (len(correct) + len(missed)) == 0:
-                        current_result.append('NA')
-                    else:
-                        current_result.append(len(correct) / (len(correct) + len(missed)))
-                    current_result.append(levenshtein_tests(trans_test, gt_test))
-                    writer.writerow(current_result)
+# def evaluate_atm_mapping_signin_signup(src_app, tgt_app):
+#     with open(os.path.join('/Users/yixue/Documents/Research/FrUITeR/Develop/ProcessedTest_CSV/', src_app + '.csv'), 'r') as test_input:
+#         with open('final_results_atm.csv', 'a') as result_output:
+#             writer = csv.writer(result_output, lineterminator='\n')
+#             reader = csv.reader(test_input)
+#             for row in reader:
+#                 src_test = []
+#                 trans_test = []
+#                 correct = []
+#                 incorrect = []
+#                 missed = []
+#                 nonExist = []
+#                 # only evaluate sign in and sign up
+#                 if 'testSignIn()' in row[0] or 'testSignUp()' in row[0]:
+#                     event_array = json.loads(row[1])
+#                     for event in event_array:
+#                         if 'id@' in event['id_or_xpath']:
+#                             src_id = event['id_or_xpath'].split('id@')[1]
+#                             src_test.append(src_id)
+#                             src_can = find_canonical_for_src(src_id, src_app)
+#                             trans_id = get_trans_event(src_id, src_app, tgt_app)
+#                             trans_can = find_canonical_for_src(trans_id, tgt_app)
+#                             trans_test.append(trans_id)
+#                             if trans_can != 'NONE':
+#                                 if src_can == trans_can:
+#                                     correct.append(src_id)
+#                                 else:
+#                                     incorrect.append(src_id)
+#                             else:
+#                                 if check_canonical(src_can, tgt_app):
+#                                     # missed case
+#                                     missed.append(src_id)
+#                                 else:
+#                                     #nonExist case
+#                                     nonExist.append(src_id)
+#                         else:
+#                             src_test.append(event['id_or_xpath'])
+#                             print ('no id found for event ', event)
+#
+#                     # calcuate TP, FP, FN by comparing transferred test with ground-truth test
+#                     method_name = str(row[0]).split(': ')[1]
+#                     gt_test = get_gt_test(tgt_app, method_name)
+#                     print ('method = ', row[0])
+#                     print ('gt test = ', gt_test)
+#                     print ('trans test = ', trans_test)
+#                     TP = set(trans_test) & set(gt_test)
+#                     FP = set(trans_test) - set(gt_test)
+#                     FN = set(gt_test) - set(trans_test)
+#                     # output the current test case's results to the file
+#                     current_result = []
+#                     current_result.append(row[0])
+#                     current_result.append(src_test)
+#                     current_result.append(trans_test)
+#                     current_result.append(gt_test)
+#                     current_result.append(src_app)
+#                     current_result.append(tgt_app)
+#                     current_result.append('atm')
+#                     current_result.append(correct)
+#                     current_result.append(incorrect)
+#                     current_result.append(missed)
+#                     current_result.append(nonExist)
+#                     current_result.append(TP)
+#                     current_result.append(FP)
+#                     current_result.append(FN)
+#                     current_result.append(len(correct))
+#                     current_result.append(len(incorrect))
+#                     current_result.append(len(missed))
+#                     current_result.append(len(nonExist))
+#                     if (len(correct) + len(incorrect)) == 0:
+#                         current_result.append('NA')
+#                     else:
+#                         current_result.append(len(correct)/(len(correct) + len(incorrect)))
+#                     if (len(correct) + len(missed)) == 0:
+#                         current_result.append('NA')
+#                     else:
+#                         current_result.append(len(correct) / (len(correct) + len(missed)))
+#                     current_result.append(levenshtein_tests(trans_test, gt_test))
+#                     writer.writerow(current_result)
 
 # trans_test is an array of trans_event where each event has keys 'id', 'class', 'bounds' or can be {}
 # gt_test is an array of 'id_or_xpath'
@@ -304,7 +304,7 @@ def convert_gt_test(gt_test, tgt_app):
 # output final results for each app pair (will append to the file if run multiple times)
 def evaluate_atm_mapping(src_app, tgt_app):
     with open(os.path.join('/Users/yixue/Documents/Research/FrUITeR/Develop/ProcessedTest_CSV/', src_app + '.csv'), 'r') as test_input:
-        with open('/Users/yixue/Documents/Research/FrUITeR/Results/ATM/atm_json.csv', 'a') as result_output:
+        with open('/Users/yixue/Documents/Research/FrUITeR/Results/ATM/atm.csv', 'a') as result_output:
             writer = csv.writer(result_output, lineterminator='\n')
             reader = csv.reader(test_input)
             next(reader) # skip the header
@@ -343,15 +343,15 @@ def evaluate_atm_mapping(src_app, tgt_app):
                 # calcuate TP, FP, FN by comparing transferred test with ground-truth test
                 method_name = str(row[0]).split(': ')[1] # get method name of the test case
                 gt_test = get_gt_test(tgt_app, method_name)
-                print ('method = ', row[0])
-                print ('gt test = ', gt_test)
-                print ('trans test = ', trans_test)
+                # print ('method = ', row[0])
+                # print ('gt test = ', gt_test)
+                # print ('trans test = ', trans_test)
                 # TP is an array that contains 'id_or_xpath'
-                TP = calculate_TP(trans_test, gt_test, tgt_app)
+                # TP = calculate_TP(trans_test, gt_test, tgt_app)
                 # FP is an array that contains trans_event dictionary that has 'id', 'class', 'bounds'
-                FP = calculate_FP(trans_test, gt_test, tgt_app)
+                # FP = calculate_FP(trans_test, gt_test, tgt_app)
                 # FN is an array that contains 'id_or_xpath'
-                FN = calculate_FN(trans_test, gt_test, tgt_app)
+                # FN = calculate_FN(trans_test, gt_test, tgt_app)
                 # output the current test case's results to the file
                 current_result = []
                 current_result.append(row[0])
@@ -365,9 +365,9 @@ def evaluate_atm_mapping(src_app, tgt_app):
                 current_result.append(incorrect)
                 current_result.append(missed)
                 current_result.append(nonExist)
-                current_result.append(TP)
-                current_result.append(FP)
-                current_result.append(FN)
+                # current_result.append(TP)
+                # current_result.append(FP)
+                # current_result.append(FN)
                 current_result.append(len(correct))
                 current_result.append(len(incorrect))
                 current_result.append(len(missed))
@@ -380,7 +380,13 @@ def evaluate_atm_mapping(src_app, tgt_app):
                     current_result.append('NA')
                 else:
                     current_result.append(len(correct) / (len(correct) + len(missed)))
-                current_result.append(levenshtein(trans_test, gt_test))
+                if (len(correct) + len(incorrect) + len(missed) + len(nonExist) ) == 0:
+                    current_result.append('NA')
+                else:
+                    current_result.append((len(correct) + len(nonExist)) /
+                                          ((len(correct) + len(incorrect) + len(missed) + len(nonExist))))
+
+                current_result.append(levenshtein_tests(trans_test, gt_test, tgt_app))
                 writer.writerow(current_result)
 
 
@@ -564,7 +570,7 @@ def evaluate_atm_mapping_batch():
 
 if __name__ == "__main__":
     # evaluate_atm_mapping_signin_signup(src_app, tgt_app)
-    # result is in 'final_results_atm.csv' with the header 'method,src_events,transferred,gt_events,source,target,gui_mapper,correct,incorrect,missed,nonExist,TP,FP,FN,num_correct,num_incorrect,num_missed,num_nonExist,accuracy_precision,accuracy_recall,distance'
+    # result is in 'final_results_atm.csv' with the header 'method,src_events,transferred,gt_events,source,target,gui_mapper,correct,incorrect,missed,nonExist,num_correct,num_incorrect,num_missed,num_nonExist,accuracy_precision,accuracy_recall,accuracy,distance'
     # a = ['aa', 'bbb', 'ccc']
     # b = ['bb', 'aa', 'ccc']
     evaluate_atm_mapping_batch()
